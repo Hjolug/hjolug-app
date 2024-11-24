2f574eac10f51b9e21b9ed04cc68f84719dbf1d5
Klara Ahlin <k.ahlin@Klaras-MacBook-Pro.local>
2024-11-24 17:49:42 +1100

    label styling tweaks

--- src/components/ReservoirGraph.js
+++ src/components/ReservoirGraph.js
@@ -11,7 +11,7 @@ const CustomTooltip = ({ active, payload, label, coordinate }) => {
 
     return (
       <div
-        className="bg-white shadow-md p-2 rounded text-sm text-black w-40"
+        className="bg-white shadow-sm p-2 items-center rounded-full text-sm text-black w-40 justify-center"
         style={{
           position: "absolute",
           top: "10px",
@@ -20,20 +20,19 @@ const CustomTooltip = ({ active, payload, label, coordinate }) => {
           pointerEvents: "none",
         }}
       >
-        <div className="space-x-2 flex flex-row">
-          <span className="font-medium text-lg flex">
+        <div className="space-x-1 flex flex-row justify-center items-center">
+          <div className="justify-center text-center text-xxs">{label}</div>
+          <span className="text-lg flex">
             {reservoirLevel}
-            <span className="font-medium text-xxs pl-1"> cm</span>
+            <span className="font-medium text-xxs pl-0.5"> cm</span>
           </span>
 
-          <span className="font-medium text-lg">/</span>
-          <span className="font-medium  text-lg flex">
+          <span className="text-lg">/</span>
+          <span className="text-lg flex">
             {powerGenerator}
-            <span className="font-medium text-xxs pl-1"> kW</span>
+            <span className="font-medium text-xxs pl-0.5"> kW</span>
           </span>
         </div>
-
-        {label}
       </div>
     );
   }
@@ -93,7 +92,7 @@ const ReservoirGraph = () => {
       <ResponsiveContainer width="100%" height="100%">
         <LineChart
           data={formattedData}
-          margin={{ top: 100, right: 0, left: 0, bottom: 0 }}
+          margin={{ top: 110, right: 0, left: 0, bottom: 0 }}
         >
           <XAxis
             dataKey="time"
